7741d7178c4fc9f509da3773732dbdcacc017c18
Ramiro Nascimento <ramirobnascimento@hotmail.com>
2023-03-27 20:33:10 -0300

    :tada: base structure

---
+++ oath/src/main/java/constsw20231/backendg5/oath/OathApplication.java
@@ -0,0 +1,13 @@
+package constsw20231.backendg5.oath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OathApplication.class, args);
+	}
+
+}
---
+++ oath/src/test/java/constsw20231/backendg5/oath/OathApplicationTests.java
@@ -0,0 +1,13 @@
+package constsw20231.backendg5.oath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
