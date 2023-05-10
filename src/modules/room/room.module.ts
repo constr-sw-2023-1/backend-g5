20d8eefaf9df75833e1344533462173b1b870dab
henriquezanette <henriquezanette1@gmail.com>
2023-05-10 19:33:59 -0300

    add http import to room module

--- src/modules/room/room.module.ts
+++ src/modules/room/room.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { RoomController } from './room.controller';
+import { HttpModule } from '@nestjs/axios';
+
 
 @Module({
+  imports: [HttpModule],
   providers: [RoomService],
   controllers: [RoomController]
 })
-export class RoomModule {}
+export class RoomModule { }
