bc8f6634ad6169753a656942cd5d0be2b409c4fa
Henrique Zanette <henriquezanette1@gmail.com>
2023-05-14 19:11:18 -0300

    add dependencies and change types

--- src/modules/room/room.service.ts
+++ src/modules/room/room.service.ts
@@ -2,31 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { CreateRoomRequestDTO } from './dto/CreateRoomRequestDTO.model';
 import { UpdateRoomRequestDTO } from './dto/UpdateRoomRequestDTO.model';
 import { UpdateRoomResourceRequestDTO } from './dto/UpdateRoomResourceRequestDTO.model';
+import { UUID } from 'crypto';
 
 @Injectable()
 export class RoomService {
-    disableRoom(roomId: string) {
-        //ToDo
-        return roomId;
-    }
-    updateRoomResource(roomId: string, newResources: UpdateRoomResourceRequestDTO) {
-        //ToDo
-        return newResources;
-    }
-    updateRoom(roomId: string, udpatedRoom: UpdateRoomRequestDTO) {
-        //ToDo
-        return udpatedRoom;
-    }
-    getRoomsById(id: string) {
-        //ToDo
-        return id;
-    }
-    createNewRoom(room: CreateRoomRequestDTO) {
-        //ToDo
-        return room;
-    }
-    getAllRooms() {
-        //ToDo
-        throw new Error('Method not implemented.');
-    }
+  disableRoom(roomId: UUID) {
+    //ToDo
+    return roomId;
+  }
+  updateRoomResource(roomId: UUID, newResources: UpdateRoomResourceRequestDTO) {
+    //ToDo
+    return newResources;
+  }
+  updateRoom(roomId: string, udpatedRoom: UpdateRoomRequestDTO) {
+    //ToDo
+    return udpatedRoom;
+  }
+  getRoomsById(id: UUID) {
+    //ToDo
+    return id;
+  }
+  createNewRoom(room: CreateRoomRequestDTO) {
+    //ToDo
+    return room;
+  }
+  getAllRooms() {
+    //ToDo
+    throw new Error('Method not implemented.');
+  }
 }
