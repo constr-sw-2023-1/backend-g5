c38ca8173f851a2faf56ed7e4b7565d4352ed991
Henrique Zanette <henriquezanette1@gmail.com>
2023-05-14 19:21:48 -0300

    add keycloak configuration and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { RoomModule } from './modules/room/room.module';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  RoleGuard,
+} from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
+
+const keyCloakOptionsProvider = {
+  provide: 'keyCloakDataProvider',
+  useFactory: (config: ConfigService) => {
+    return {
+      authServerUrl: config.get('KEYCLOAK_AUTH_URI'),
+      realm: config.get('KEYCLOAK_REALM'),
+      clientId: config.get('KEYCLOAK_CLIENT_ID'),
+      secret: config.get('KEYCLOAK_CLIENT_SECRET'),
+    };
+  },
+  inject: [ConfigService],
+};
 
 @Module({
   imports: [
+    KeycloakConnectModule.registerAsync(keyCloakOptionsProvider),
     RoomModule,
     HttpModule,
     ConfigModule.forRoot({
@@ -12,6 +33,15 @@ import { ConfigModule } from '@nestjs/config';
     }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
