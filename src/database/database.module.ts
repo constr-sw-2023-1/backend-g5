2c6b8e1827d4176beacf0a3e1c30ebd1daf13d04
henriquezanette <henriquezanette1@gmail.com>
2023-05-15 20:16:12 -0300

    fix mongo url connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://database/nest')],
+  imports: [MongooseModule.forRoot('mongodb://root:root@database/writeapp?authSource=admin')],
   controllers: [],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
