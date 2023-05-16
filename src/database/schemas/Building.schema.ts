3f68c51cc572d95d9918033a8aba0f7be57cbd6a
henriquezanette <henriquezanette1@gmail.com>
2023-05-15 21:58:49 -0300

    fix exports and implementations

--- src/database/schemas/Building.schema.ts
+++ src/database/schemas/Building.schema.ts
@@ -1,9 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Building as IBuilding } from '../../modules/room/models/Building.model';
 
 export type BuildingDocument = Building & Document;
 
 @Schema()
-export class Building {
+export class Building implements IBuilding {
   @Prop()
   id: string;
 
--- src/database/schemas/Room.schema.ts
+++ src/database/schemas/Room.schema.ts
@@ -1,10 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Resource } from './Resource.schema';
+import { Room as IRoom } from '../../modules/room/models/Room.model';
 
 export type RoomDocument = Room & Document;
 
 @Schema()
-export class Room {
+export class Room implements IRoom {
   @Prop()
   id: string;
 
